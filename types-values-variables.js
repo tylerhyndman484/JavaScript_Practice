50f33905c482902a61845f783212bef0815bb22b
Tyler Hyndman <tylerhyndman484@hotmail.com>
2024-10-19 09:29:32 -0500

    practice

--- types-values-variables.js
+++ types-values-variables.js
@@ -4,9 +4,12 @@ let number = 5;
 
 console.log(Math.cbrt(90));
 
+let billion = 1_000_000_000_000;
 
+console.log(billion);
 
+// math object //
 
-
+console.log(Math.random(Math.ceil()));
 
 /* NUMBERS  */
