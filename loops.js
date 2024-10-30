6f2deb7b9e26211aa887fae67c069fde9b8773fc
Tyler Hyndman <tylerhyndman484@hotmail.com>
2024-10-29 23:39:12 -0500

    started whale project on codecademy

---
+++ loops.js
@@ -0,0 +1,18 @@
+// For loops //   // while loops //   // do... while lopps //
+
+// Whale Project // 
+
+// whales only speak with "a, e, i, o, u";
+
+const input = "Jesus is Lord";
+const vowels = ['a', 'e', 'i', 'o', 'u'];
+const resultArray = [];
+
+for (let i = 0; i < input.length; i++) {
+  for ( let j = 0; j < vowels.length; j++) {
+    
+  }
+  // console.log(input[i]);
+}
+
+// Whale Project //
