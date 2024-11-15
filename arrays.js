559980fe7e9781849f34edd63a9ee2ad5426daca
Tyler Hyndman <tylerhyndman484@hotmail.com>
2024-11-15 13:21:57 -0600

    practicing code challenges on codecademy

--- arrays.js
+++ arrays.js
@@ -156,3 +156,20 @@ function greetAliens(array) {
 greetAliens(aliens);
 
 
+
+// Write your code here:
+function convertToBaby(array) {
+  let newArray = [];
+  for (let i = 0; i < array.length; i++) {
+    newArray.push(`baby ${array[i]}`);
+  }
+  return newArray;
+}
+
+// When you're ready to test your code, uncomment the below and run:
+
+const animals = ['panda', 'turtle', 'giraffe', 'hippo', 'sloth', 'human'];
+
+console.log(convertToBaby(animals)) 
+
+
