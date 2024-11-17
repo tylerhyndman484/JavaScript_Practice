d68aee8a071091744092d6a6fddf19a8d0bbf8d6
Tyler Hyndman <tylerhyndman484@hotmail.com>
2024-11-16 23:26:59 -0600

    practicing on codecademy

--- arrays.js
+++ arrays.js
@@ -170,6 +170,26 @@ function convertToBaby(array) {
 
 const animals = ['panda', 'turtle', 'giraffe', 'hippo', 'sloth', 'human'];
 
-console.log(convertToBaby(animals)) 
+console.log(convertToBaby(animals));
+
+const smallestPowerOfTwo = arr => {
+  let results = [];
+  // The 'outer' for loop - loops through each element in the array
+  for (let i = 0; i < arr.length; i++) {
+        number = arr[i];
+        // The 'inner' while loop - searches for smallest power of 2 greater than the given number
+        j = 1;
+        while (j < number) {
+              j = j * 2;
+        }
+        results.push(j);
+  }
+  return results
+}
+
+smallestPowerOfTwo(numbers); 
+// Should print the returned array [ 8, 4, 16, 32 ] instead prints the returned array [8]
 
 
+
+// practicing on codecademy // 
